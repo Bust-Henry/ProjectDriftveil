56f5076d416dca017156787c2a311a0c4fe10899
Henry Bust <Henry.Bust@hotmail.de>
2022-12-10 23:49:00 +0100

    added dex scraper

---
+++ dexScraper.py
@@ -0,0 +1,24 @@
+import requests
+import os
+
+def getPokemon(number):
+    response = requests.get(os.environ.get("api") + "pokemon/" + str(number))
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+def getAllPokemon(startAt:int=1):
+    i = startAt
+    pokemonList = []
+    while True:
+        pokemon = getPokemon(i)
+        if pokemon is not None:
+            pokemonList.append(pokemon)
+            i+=1
+        else:
+            break
+    return pokemonList
+
+if __name__=="__main__": 
+    os.environ["api"] = "https://pokeapi.co/api/v2/"
+    print(getAllPokemon())
