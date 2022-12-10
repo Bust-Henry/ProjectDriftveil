4246d460a1d13ff8cf83ab7699d23cc09307ac5f
Henry Bust <Henry.Bust@hotmail.de>
2022-12-10 23:50:02 +0100

    created bluestack controller

---
+++ blueStackController.py
@@ -0,0 +1,16 @@
+import imageRecognition
+class BlueStackController():
+    
+    def __init__(self) -> None:
+        pass
+
+    def countPokemon(self):
+        lastPokemonStats = None
+        currentPokemonStats = None
+        # get current pokemon
+
+        # move to next pokemon
+
+        # check if same pokemon
+    
+        # if same pokemon twice in a row, counting completed
