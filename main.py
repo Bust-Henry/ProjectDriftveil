01aa720c50c2227e0d1d57af6107f5185bf6f85f
Henry Bust <Henry.Bust@hotmail.de>
2022-12-11 20:19:13 +0100

    added argparser to rebuild database if needed

--- main.py
+++ main.py
@@ -4,13 +4,29 @@ import dexScraper
 from dbconnector import DBConnector, PokemonElement
 import imageRecognition
 from blueStackController import BlueStackController
-import PIL
+import argparse
 
 def loadConfig(path):
     with open(path, "r") as jsonfile:
         return json.load(jsonfile)
 
+def rebuild_database():
+    print("rebuilding database, this might take a while")
+    db = DBConnector(os.environ.get("dbpath"))
+    db.createDB()
+    pokemonList = dexScraper.getAllPokemon()
+    for pokemon in pokemonList:
+        db.add(PokemonElement(pokemon["id"], pokemon["species"]["name"], 0, 0))
+    print("done")
+
 if __name__=="__main__":
+    # --------------------------argument parser options------------------------
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r','--rebuild', action='store_true', help="rebuild the pokemon database")
+    parser.add_argument('-c','--calibrate', action='store_true', help="calibrate the areas of text")
+    args = parser.parse_args()
+
+    # --------------------------environment variables------------------------
     # setting environment variables
     os.environ['config'] = os.path.join(os.path.dirname(__file__), "data", "config.json")
     # loading config file
@@ -20,21 +36,16 @@ if __name__=="__main__":
         os.environ[item[0]] = item[1]
     
     # ------------------ Start Script -------------------
-    """
-    db = DBConnector(os.environ.get("dbpath"))
-    db.createDB()
-    #print(dexScraper.getHTML(os.environ.get("dexlink")))
-    pokemonList = dexScraper.getAllPokemon()
-    for pokemon in pokemonList:
-        db.add(PokemonElement(pokemon["id"], pokemon["species"]["name"], 0, 0))
-    print("done")"""
+
+    # if script startet with --rebuild flag, the database will be rebuilt.
+    if args.rebuild:
+        rebuild_database()
+
     controller = BlueStackController()
     controller.setForeGround()
     controller.screenshot()
     import time
-    time.sleep(2)
+    time.sleep(1)
     controller.renameScreenshot("screenshot.png")
     print(imageRecognition.readBluestacks())
-    print("screenshot taken, analyze...")
-    time.sleep(3)
     controller.deleteScreenshot()
