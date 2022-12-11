76ecf2610e933b1497d96e3022457258ad162108
Henry Bust <Henry.Bust@hotmail.de>
2022-12-11 19:44:11 +0100

    initialized the blueStackController class

--- blueStackController.py
+++ blueStackController.py
@@ -1,8 +1,41 @@
 import imageRecognition
+import pyautogui
+import win32gui
+import os
+
 class BlueStackController():
-    
+    toplist, winlist = [], []
+    def enum_cb(self, hwnd, results):
+        self.winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+   
+    bluestacksHandle = None
+    screenshotPath = None
     def __init__(self) -> None:
-        pass
+        self.screenshotPath = os.path.join(os.path.dirname(__file__), os.environ.get("screenshot"))
+        
+        
+    def setForeGround(self):
+        win32gui.EnumWindows(self.enum_cb, self.toplist)
+        bluestack = [(hwnd, title) for hwnd, title in self.winlist if 'bluestacks' in title.lower()]
+        # just grab the hwnd for first window matching bluestack
+        bluestack = bluestack[0]
+        hwnd = bluestack[0]
+        win32gui.SetForegroundWindow(hwnd)
+
+    def screenshot(self):
+        pyautogui.hotkey('ctrl','shift','s')
+
+    def renameScreenshot(self, name):
+        for file in os.listdir(self.screenshotPath):
+            if file.endswith(".png"):
+                os.rename(os.path.join(self.screenshotPath, file), os.path.join(self.screenshotPath, name))
+    
+    def deleteScreenshot(self):
+        try:
+            os.remove(os.path.join(self.screenshotPath, "screenshot.png"))
+        except FileNotFoundError:
+            print("screenshot wasn't deleted, becaus it doesnt exist")
+
 
     def countPokemon(self):
         lastPokemonStats = None
@@ -14,3 +47,8 @@ class BlueStackController():
         # check if same pokemon
     
         # if same pokemon twice in a row, counting completed
+
+if __name__=="__main__":
+    controller = BlueStackController()
+    controller.setForeGround()
+    controller.deleteScreenshot()
