e57034d5b1ea153be1137f70ed34b9a8642c1810
Henry Bust <Henry.Bust@hotmail.de>
2022-12-11 22:33:25 +0100

    clearRegisteredPokemon and registerePokemon added

--- dbconnector.py
+++ dbconnector.py
@@ -38,3 +38,27 @@ class DBConnector:
             self.engine.execute(self.tab.insert(), nr=pokemon.nr, name=pokemon.name, owned=pokemon.owned, shinyowned=pokemon.shinyowned)
         except IntegrityError:
             print(pokemon.toString(), "already exists. Skipping...")
+        
+    def clearRegisteredPokemon(self):
+        max = len(self.engine.execute(self.tab.select()).fetchall())
+        for i in range(1, max+1):    
+            self.engine.execute(self.tab.update().where(self.tab.c.nr==i).values(owned=0, shinyowned=0))
+
+    def registerPokemon(self, number, shiny=False):
+        try:
+            if not shiny:
+                result = self.engine.execute(self.tab.select().where(self.tab.c.nr==number)).fetchone()
+                if result == None:
+                    print("pokemon nr. " + number + " doesnt exists in this database")
+                    return False
+                currentlyowned = result[3]
+                self.engine.execute(self.tab.update().where(self.tab.c.nr==number).values(owned=currentlyowned+1))
+            else:
+                result = self.engine.execute(self.tab.select().where(self.tab.c.nr==number)).fetchone()
+                if result == None:
+                    print("pokemon nr. " + number + " doesnt exists in this database")
+                    return False
+                currentlyowned = result[4]
+                self.engine.execute(self.tab.update().where(self.tab.c.nr==number).values(shinyowned=currentlyowned+1))
+        except IntegrityError:
+            print("IntegrityError")
