b90efbba013d8c48b3d8cd6a642603c07fa6ace8
Henry Bust <Henry.Bust@hotmail.de>
2022-12-25 00:59:58 +0100

    changed path to fit the actual save file path

--- fileSender.py
+++ fileSender.py
@@ -5,7 +5,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Connect to the server.
 sock.connect(("192.168.178.100", 5050))
 
-with open("C:/Users/Henry/Desktop/34be958a921e43d813a2075297d8e862", "rb") as file:
+with open("/sdcard/Android/data/jp.pokemon.pokemonhome/files/utsudon/34be958a921e43d813a2075297d8e862", "rb") as file:
     sock.sendfile(file)
 
 sock.close()
+print("done!")
