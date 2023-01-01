53afcddfb2b1114124c2ad9b51596089b4340141
Henry Bust <Henry.Bust@hotmail.de>
2023-01-01 18:26:24 +0100

    added the discord bot to launches

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 if __name__ == "__main__":
     api_path=os.environ.get("api_path")
     homeConnector_path=os.environ.get("homeConnector_path")
+    discordBot_path=os.environ.get("discordBot_path")
     if not os.path.exists(api_path):
         print("Missing file:", api_path)
         print("Press enter to continue...")
@@ -16,5 +17,11 @@ if __name__ == "__main__":
         print("Press enter to continue...")
         input()
         exit()
+    if not os.path.exists(discordBot_path):
+        print("Missing file:", discordBot_path)
+        print("Press enter to continue...")
+        input()
+        exit()
     Popen(f"cmd /K python3 {api_path}", creationflags=CREATE_NEW_CONSOLE)
     Popen(f"cmd /K python3 {homeConnector_path}", creationflags=CREATE_NEW_CONSOLE)
+    Popen(f"cmd /K python3 {discordBot_path}", creationflags=CREATE_NEW_CONSOLE)
